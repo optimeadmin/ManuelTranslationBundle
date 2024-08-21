70fa56d07bb9a23dcac3d25726f39e910c1380c8
Manuel Aguirre <programador.manuel@gmail.com>
2024-08-21 16:29:52 -0500

    Update DoctrineLoader.php

--- Translation/Loader/DoctrineLoader.php
+++ Translation/Loader/DoctrineLoader.php
@@ -11,6 +11,7 @@
 namespace ManuelAguirre\Bundle\TranslationBundle\Translation\Loader;
 
 use ManuelAguirre\Bundle\TranslationBundle\TranslationRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\Translation\Exception\InvalidResourceException;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
@@ -27,33 +28,74 @@ class DoctrineLoader implements LoaderInterface
      * @var TranslationRepository
      */
     protected $translationRepository;
+    /**
+     * @var TranslationRepository
+     */
+    protected $backupTranslationRepository;
     protected $fileTemplate;
+    /**
+     * @var LoggerInterface|null
+     */
+    protected $logger = null;
 
-    function __construct($translationRepository, $fileTemplate)
-    {
+    public function __construct(
+        TranslationRepository $translationRepository,
+        TranslationRepository $backupTranslationRepository,
+        string $fileTemplate,
+        ?LoggerInterface $logger = null
+    ) {
         $this->translationRepository = $translationRepository;
+        $this->backupTranslationRepository = $backupTranslationRepository;
         $this->fileTemplate = $fileTemplate;
+        $this->logger = $logger;
     }
 
     /**
      * Loads a locale.
      *
-     * @param mixed  $resource A resource
-     * @param string $locale   A locale
-     * @param string $domain   The domain
+     * @param mixed $resource A resource
+     * @param string $locale A locale
+     * @param string $domain The domain
      *
      * @return MessageCatalogue A MessageCatalogue instance
      *
-     * @api
-     *
      * @throws NotFoundResourceException when the resource cannot be found
      * @throws InvalidResourceException  when the resource cannot be loaded
+     * @api
+     *
      */
     public function load($resource, $locale, $domain = 'messages')
     {
-        $translations = $this->translationRepository->getActiveTranslations();
-
         $catalogue = new MessageCatalogue($locale);
+        $catalogue->addResource(new FileResource($resource));
+
+        try {
+            $translations = $this->translationRepository->getActiveTranslations();
+        } catch (\Throwable $e) {
+            if ($this->logger) {
+                $this->logger->warning('No se pudo obtener las traducciones desde la base de datos', [
+                    'error' => $e->getMessage(),
+                    'error_type' => get_class($e),
+                ]);
+            }
+
+            try {
+                if ($this->logger) {
+                    $this->logger->warning('Intentando cargar las traducciones desde el backup de traducciones');
+                }
+
+                $translations = $this->backupTranslationRepository->getActiveTranslations();
+            } catch (\Throwable $e) {
+                if ($this->logger) {
+                    $this->logger->warning('No se pudo obtener las traducciones desde el backup', [
+                        'error' => $e->getMessage(),
+                        'error_type' => get_class($e),
+                    ]);
+                }
+
+                return $catalogue;
+            }
+        }
 
         foreach ($translations as $translation) {
             if (array_key_exists($locale, $translation['values'])) {
@@ -61,8 +103,6 @@ class DoctrineLoader implements LoaderInterface
                 $catalogue->set($code, $translation['values'][$locale], $translation['domain']);
             }
         }
-
-        $catalogue->addResource(new FileResource($resource));
 
         return $catalogue;
     }
